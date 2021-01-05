132b44763007250a77e9189e3e115268054c13f4
kunxin-chor <chorkunxin@yahoo.com>
2021-01-05 06:11:13 +0000

    Added migrations

--- oldbooks/app/Controllers/Http/BookController.js
+++ oldbooks/app/Controllers/Http/BookController.js
@@ -1,35 +1,22 @@
 'use strict'
 
-const booksDB = [
-  {
-    'id': 1,
-    'title': 'Lord of the Rings',
-    'condition': 3
-  },
-  {
-    'id': 2,
-    'title': 'A Wizard in Rhyme',
-    'condition': 2
-  },
-  {
-    'id': 3,
-    'title': 'Waylander',
-    'condition': 2.5
-  }
 
-]
+const Book = use('App/Models/Book')
 
 class BookController {
-  index({view}) {
+  async index({view}) {
+    // select all the rows from the books table
+    let allBooks = await Book.all();
     return view.render('books/index', {
-      "books": booksDB
+      "books": allBooks.toJSON() // must convert to JSON
     })
   }
 
-  show({view, params}) {
+  async show({view, params}) {
     // extract out the book_id parameter from the URL
     let bookId = params.book_id;
-    let book = booksDB.find( b => b.id === parseInt(bookId));
+    // select * from books where id = bookId
+    let book = await Book.find(bookId);
     return view.render("books/show", {
       "book": book
     })
